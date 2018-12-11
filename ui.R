1ed2da5853ed497ca756ccdaf268da141e58ff0f
Kyle David Doherty <kyledaviddoherty@gmail.com>
2018-12-10 20:37:49 -0700

    point ui to new logos

--- ui.R
+++ ui.R
@@ -5,7 +5,8 @@ ui <- fluidPage(
     sidebarPanel(width=2,
                  
                  fluidRow(
-                   img(src="https://storage.googleapis.com/seedmapper_dat/usgs.log.png", height=81, width='100%'),
+                   img(src="https://github.com/mosscoder/climpart/blob/master/cpnpp_logo.png?raw=true", height=81, width='100%'),
+                   img(src="https://github.com/mosscoder/climpart/blob/master/usgs.logo.png?raw=true", height=81, width='100%'),
                    
                    tags$div(id = 'selectDiv', selectInput("boundSelect", 
                                                           label = ("How would you like to define your area of interest?"), 
